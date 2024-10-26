646a86e38a5d5aec16eb34272406362a22a2afa6
Azher Bashir <125378433+Azher-Bashir@users.noreply.github.com>
2024-10-27 01:20:14 +0500

    Add files via upload

---
+++ ICT assignment 02/ICT LAB/QUESTION # 07 _ TO CHECK WETHER THE YEAR IS LEAP OR NOT.cpp
@@ -0,0 +1,23 @@
+////QUESTION # 07 _ Write a C++ program to determine if a year is a leap year using an if-
+//// else statement.
+//
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    int year;
+//    cout << "Enter the year : ";
+//    cin >> year;
+//    if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+//    {
+//        cout << "The entered year is leap_year" << endl;
+//    }
+//    else
+//    {
+//        cout << "The enters year is not leap_year" << endl;
+//    }
+//     system("pause");
+//    return 0;
+//}
