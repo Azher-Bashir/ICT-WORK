5d869b57c7d59addab495269f909e909bdc9dbe9
Azher Bashir <azherbashir07@gmail.com>
2024-10-27 21:48:42 +0500

    Nothing

--- ICT assignment 02/QUESTION # 10 _ IDENTIFY THE CHARACTER TYPE.cpp
+++ ICT assignment 02/QUESTION # 10 _ IDENTIFY THE CHARACTER TYPE.cpp
@@ -25,67 +25,76 @@
 //    case 'U':
 //        cout << "The character is a vowel." << endl;
 //        break;
-//    case 'b':
-//    case 'c':
-//    case 'd':
-//    case 'f':
-//    case 'g':
-//    case 'h':
-//    case 'j':
-//    case 'k':
-//    case 'l':
-//    case 'm':
-//    case 'n':
-//    case 'p':
-//    case 'q':
-//    case 'r':
-//    case 's':
-//    case 't':
-//    case 'v':
-//    case 'w':
-//    case 'x':
-//    case 'y':
-//    case 'z':
-//        cout << "The character is a consonant." << endl;
-//        break;
-//    case 'B':
-//    case 'C':
-//    case 'D':
-//    case 'F':
-//    case 'G':
-//    case 'H':
-//    case 'J':
-//    case 'K':
-//    case 'L':
-//    case 'M':
-//    case 'N':
-//    case 'P':
-//    case 'Q':
-//    case 'R':
-//    case 'S':
-//    case 'T':
-//    case 'V':
-//    case 'W':
-//    case 'X':
-//    case 'Y':
-//    case 'Z':
-//        cout << "The character is a consonant." << endl;
-//        break;
-//    case '1':
-//    case '2':
-//    case '3':
-//    case '4':
-//    case '5':
-//    case '6':
-//    case '7':
-//    case '8':
-//    case '9':
-//    case '0':
-//        cout << "The character is a number." << endl;
 //    default:
-//        cout << "The character is a special character." << endl;
-//        break;
+//        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+//        {
+//            cout << ch << " is an alphabet." << endl;
+//        }
+//        else
+//        {
+//            cout << ch << " is a special character" << endl;
+//        }
 //    }
 //     system("pause");
 //    return 0;
 //}
+//    //case 'b':
+//    //case 'c':
+//    //case 'd':
+//    //case 'f':
+//    //case 'g':
+//    //case 'h':
+//    //case 'j':
+//    //case 'k':
+//    //case 'l':
+//    //case 'm':
+//    //case 'n':
+//    //case 'p':
+//    //case 'q':
+//    //case 'r':
+//    //case 's':
+//    //case 't':
+//    //case 'v':
+//    //case 'w':
+//    //case 'x':
+//    //case 'y':
+//    //case 'z':
+//    //    cout << "The character is a consonant." << endl;
+//    //    break;
+//    //case 'B':
+//    //case 'C':
+//    //case 'D':
+//    //case 'F':
+//    //case 'G':
+//    //case 'H':
+//    //case 'J':
+//    //case 'K':
+//    //case 'L':
+//    //case 'M':
+//    //case 'N':
+//    //case 'P':
+//    //case 'Q':
+//    //case 'R':
+//    //case 'S':
+//    //case 'T':
+//    //case 'V':
+//    //case 'W':
+//    //case 'X':
+//    //case 'Y':
+//    //case 'Z':
+//    //    cout << "The character is a consonant." << endl;
+//    //    break;
+//    //case '1':
+//    //case '2':
+//    //case '3':
+//    //case '4':
+//    //case '5':
+//    //case '6':
+//    //case '7':
+//    //case '8':
+//    //case '9':
+//    //case '0':
+//    //    cout << "The character is a number." << endl;
+//    //default:
+//    //    cout << "The character is a special character." << endl;
+//    //    break;
