68bb678972c58e24d095a1e40bb3ecf67cab4e74
Azher Bashir <azherbashir07@gmail.com>
2024-10-27 22:05:52 +0500

    Exercise 01

---
+++ ICT LAB 10/Source1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+	int year;
+	cin << "Enter teh year";
+	cout >> year;
+	//switch (year)
+	//	case((year % 4 == 0) || (year % 100 != 0) && (year % 400 == 0)):
+	//		cout << year << " is a leap year" << endl;
+	//	default:
+	//		cout << "Wrong entry..." << endl;
+	//		system("pause");
+	//		return 0;
+}
