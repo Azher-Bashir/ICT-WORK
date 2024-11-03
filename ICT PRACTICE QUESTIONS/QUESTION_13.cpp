8db0cb18947480b7a83ce117924912c2f7f8c1da
Azher Bashir <azherbashir07@gmail.com>
2024-11-04 01:47:33 +0500

    NOTHING

--- ICT PRACTICE QUESTIONS/QUESTION_13.cpp
+++ ICT PRACTICE QUESTIONS/QUESTION_13.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
-#include <cmath>
-
-using namespace std;
-
-int main()
-{
-	int num;;
-	cout << "Enter an integer: ";
-	cin >> num;
-	cout << "Perfect squares are:";
-	for (int i = 1; i <= num; ++i)
-	{
-		if (sqrt(i) == int(sqrt(i)))
-			cout << i << ", ";
-
-	}
-	cout << endl;
-	return 0;
-
-}
+//#include <iostream>
+//#include <cmath>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int num;;
+//	cout << "Enter an integer: ";
+//	cin >> num;
+//	cout << "Perfect squares are:";
+//	for (int i = 1; i <= num; ++i)
+//	{
+//		if (sqrt(i) == int(sqrt(i)))
+//			cout << i << ", ";
+//
+//	}
+//	cout << endl;
+//	return 0;
+//
+//}
