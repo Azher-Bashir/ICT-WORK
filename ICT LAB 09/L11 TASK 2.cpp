4c48fa950c6209097e9ed3869b2c5a3a36b916c1
Azher Bashir <azherbashir07@gmail.com>
2024-11-07 01:13:43 +0500

    lab 11

---
+++ ICT LAB 09/L11 TASK 2.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//
+//{
+//	int a, b, sum = 0;
+//	cout << "Enter the lower limit: ";
+//	cin >> a;
+//	cout << "Enter the upper limit: ";
+//	cin >> b;
+//	cout << "Even numbers from " << a << " to " << b << " is : ";
+//	for (a; a <= b; a++)
+//	{
+//		if (a % 2 == 0)
+//		{
+//			cout << a << ", ";
+//			sum += a;
+//
+//		}
+//		
+//	}
+//	cout << endl << "Sum of even numbers within the given number is: " << sum << endl;
+//	return 0;
+//}
