d9e96ad97a7d3d6fe6567e274744046b0a734f09
Azher Bashir <azherbashir07@gmail.com>
2024-11-07 01:14:04 +0500

    lab 11

---
+++ ICT LAB 09/L11 TASK 3.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int a, b, fact = 1, sum = 0;
+//	cout << "Enter the value of lower limit: ";
+//	cin >> a;
+//	cout << "Enter the value of upper limit: ";
+//	cin >> b;
+//	int i = 1;
+//	cout << "Integer" << '\t' << '\t' << "Factorial" << '\t' << "sum" << endl;
+//	cout << "-------------------------------------------" << endl;
+//	cout << "-------------------------------------------" << endl;
+//	while (a <= b)
+//	{
+//		while (i <= a)
+//		{
+//			fact *= i;
+//			i++;
+//		}
+//		sum += a;
+//		cout << a << '\t' << '\t' << fact << '\t' << '\t' << sum << endl;
+//		a++;
+//	}
+//
+//	return 0;
+//}
