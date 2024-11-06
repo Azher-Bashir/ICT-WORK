41b637154dd6db4fee35646a72f949adb202e2b2
Azher Bashir <azherbashir07@gmail.com>
2024-11-07 01:48:31 +0500

    Lab 12

--- ICT LAB 09/L11 TASK 4.cpp
+++ ICT LAB 09/L11 TASK 4.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-	int i, a = 0, b = 1, sum = 0, n = 1;
-	cout << "Enter the last number: ";
-	cin >> i;
-	cout << "No." << '\t' << "Pattern" << endl;
-	while (n <= i)
-		//for (n; n <= i; n++) // for using this instead of while first comment the condition of n++ at last of while loop
-	{
-		sum = (a + b);
-		cout << n << '\t' << sum << endl;
-		b = sum;
-		a = sum - a;
-		n++;
-	}
-	return 0;
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int i, a = 0, b = 1, sum = 0, n = 1;
+//	cout << "Enter the last number: ";
+//	cin >> i;
+//	cout << "No." << '\t' << "Pattern" << endl;
+//	while (n <= i)
+//		//for (n; n <= i; n++) // for using this instead of while first comment the condition of n++ at last of while loop
+//	{
+//		sum = (a + b);
+//		cout << n << '\t' << sum << endl;
+//		b = sum;
+//		a = sum - a;
+//		n++;
+//	}
+//	return 0;
+//}
---
+++ ICT LAB 09/L12 TASK 1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	cout << "Enter which table you want to be displayted:";
+	cin >> a;
+	cout << "The next three tables are:" << endl;
+	//a = a;
+	for (int i = a; i < a + 3; i++)
+	{
+		cout << "-------------------------------------" << endl;
+		cout << "----------- TABLE OF " << i << " IS -----------" << endl;
+		cout << "-------------------------------------" << endl;
+		for (int b = 0; b <= 10; b++)
+		{
+			cout << i << " * " << b << " = " << i * b << endl;
+		}
+	}
+	return 0;
+}
