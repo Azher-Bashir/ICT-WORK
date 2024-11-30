457f11fbbe6e3bd39867678027581888fa1ba5d6
Azher Bashir <azherbashir07@gmail.com>
2024-11-30 12:13:52 +0500

    nothing

--- ICT ASSIGNMENTS/QUEATION_01.cpp
+++ ICT ASSIGNMENTS/QUEATION_01.cpp
@@ -3,29 +3,9 @@
 
 using namespace std;
 
-double radius(double circle_area);
-double circumference();
-double surface_area(double height, double circle_area);
-double volume(double height, double circle_area);
-//double surface_area(double height, double circle_area);
-
 double rad, cir, surf_area, vol, circle_area, height;
 double pi = 3.1416;
 
-int main()
-{
-	cout << "Enter the value of area of the base circle: ";
-	cin >> circle_area;
-	cout << "Enter the height of the cylinder: ";
-	cin >> height;
-
-	radius(circle_area);
-	circumference();
-	surface_area(height, circle_area);
-	volume(height, circle_area);
-	//system("pause");
-	return 0;
-}
 static double radius(double circle_area)
 {
 	// now we knoe that area = pi * r^2
@@ -54,4 +34,22 @@ static double volume(double height, double circle_area)
 	vol = circle_area * height;
 	cout << "Volume of cylinder is: ";
 	return vol;
+}
+
+
+
+
+int main()
+{
+	cout << "Enter the value of area of the base circle: ";
+	cin >> circle_area;
+	cout << "Enter the height of the cylinder: ";
+	cin >> height;
+
+	radius(circle_area);
+	circumference();
+	surface_area(height, circle_area);
+	volume(height, circle_area);
+	//system("pause");
+	return 0;
 }
