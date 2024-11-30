e10f0d1a9e8d116d489404b22eb72e757170a43c
Azher Bashir <azherbashir07@gmail.com>
2024-11-30 12:11:47 +0500

    Started working on assignment # 03

--- ICT LAB 09/L13 TASK 5.cpp
+++ ICT LAB 09/L13 TASK 5.cpp
@@ -1,29 +1,29 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-static int squareSum(int square)
-{
-	int square_sum = 0;
-	for (int i = 0; i <= 10; i++)
-	{
-		square_sum += pow(i, 2);
-	}
-	cout << "First square and then their sum is: " << square_sum << endl;
-	return square_sum;
-}
-static int sumSquare(int su)
-{
-	int sum_square = 0;
-	for (int j = 0; j <= 10; j++)
-	{
-		sum_square += j;
-	}
-	cout << "First sum and then there square is: " << pow(sum_square, 2) << endl;
-	return sum_square;
-}
-int main()
-{
-	int square = 0, su = 0;
-	cout << "Difference of them is: " << pow(sumSquare(su), 2) - squareSum(square) << endl;
-	return 0;
-}
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//static int squareSum(int square)
+//{
+//	int square_sum = 0;
+//	for (int i = 0; i <= 10; i++)
+//	{
+//		square_sum += pow(i, 2);
+//	}
+//	cout << "First square and then their sum is: " << square_sum << endl;
+//	return square_sum;
+//}
+//static int sumSquare(int su)
+//{
+//	int sum_square = 0;
+//	for (int j = 0; j <= 10; j++)
+//	{
+//		sum_square += j;
+//	}
+//	cout << "First sum and then there square is: " << pow(sum_square, 2) << endl;
+//	return sum_square;
+//}
+//int main()
+//{
+//	int square = 0, su = 0;
+//	cout << "Difference of them is: " << pow(sumSquare(su), 2) - squareSum(square) << endl;
+//	return 0;
+//}
