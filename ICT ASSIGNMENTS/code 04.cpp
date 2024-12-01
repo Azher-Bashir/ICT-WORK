9aa69246a472beab7157b73b210685566f132a70
Azher Bashir <125378433+Azher-Bashir@users.noreply.github.com>
2024-12-01 12:43:58 +0500

    Create code 04.cpp
    queation 4 was added to assignment 3

---
+++ ICT ASSIGNMENTS/code 04.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//static void ordered_pair(int start, int stop)
+//{
+//	for (int i = start; i <= stop; i++)
+//	{
+//		for (int j = i; j <= stop; j++)
+//		{
+//			cout << "(" << i << " , " << j << ")" << " ";
+//		}
+//		cout << endl;
+//	}
+//	//return;
+//}
+//int main()
+//{
+//	int start, stop;
+//	cout << "Enter the starting value: ";
+//	cin >> start;
+//	cout << "Enter thr ending value: ";
+//	cin >> stop;
+//	cout << endl;
+//	ordered_pair(start, stop);
+//	return 0;
+//}
