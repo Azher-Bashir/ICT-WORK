a6d4bcf0980508b6f70f04244c4a9c49c80c41fc
Azher Bashir <125378433+Azher-Bashir@users.noreply.github.com>
2024-12-01 12:39:51 +0500

    Create code 01.cpp
    Code 1 was added

---
+++ ICT ASSIGNMENTS/code 01.cpp
@@ -0,0 +1,56 @@
+//#include <iostream>
+//#include <cmath>
+//#include <iomanip>
+//
+//using namespace std;
+//
+//double rad, cir, surf_area, vol, circle_area, height;
+//double pi = 3.1416;
+//
+//static double radius(double circle_area)
+//{
+//	// now we knoe that area = pi * r^2
+//	rad = 0;
+//	rad = sqrt(circle_area / pi);
+//	cout << "Radius of the circle is: " << setprecision(2) << fixed << rad << endl;
+//	return rad;
+//}
+//
+//static double circumference()
+//{
+//	cir = 2 * pi * rad;
+//	cout << "circumference of the circle is: " << setprecision(2) << fixed << cir << endl;
+//	return cir;
+//}
+//
+//static double surface_area(double height, double circle_area)
+//{
+//	surf_area = (cir * height) + (2 * circle_area);
+//	cout << "Surface area of the circle is: " << setprecision(2) << fixed << surf_area << endl;
+//	return surf_area;
+//}
+//
+//static double volume(double height, double circle_area)
+//{
+//	vol = circle_area * height;
+//	cout << "Volume of cylinder is: " << setprecision(2) << fixed << vol << endl;
+//	return vol;
+//}
+//
+//
+//
+//
+//int main()
+//{
+//	cout << "Enter the value of area of the base circle: ";
+//	cin >> circle_area;
+//	cout << "Enter the height of the cylinder: ";
+//	cin >> height;
+//
+//	radius(circle_area);
+//	circumference();
+//	surface_area(height, circle_area);
+//	volume(height, circle_area);
+//	//system("pause");
+//	return 0;
+//}
