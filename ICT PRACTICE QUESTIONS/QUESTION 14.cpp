dc6b5e43b80328ad31faea34f321da8d2d6181d0
Azher Bashir <azherbashir07@gmail.com>
2024-12-26 00:26:30 +0500

    ICT FINAL PRACTICE QUEASTIONS

---
+++ ICT PRACTICE QUESTIONS/QUESTION 14.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//
+//using namespace std;
+//int lower, upper, sum = 0;
+//static int sum_of_squares(int lower, int upper)
+//{
+//	for (int i = lower; i <= upper; i++)
+//	{
+//		sum += pow(i, 2);
+//	}
+//	return sum;
+//}
+//int main()
+//{
+//	cout << "Enter the lower limit: ";
+//	cin >> lower;
+//	cout << "Enter the upper limit: ";
+//	cin >> upper;
+//
+//	sum_of_squares(lower, upper);
+//
+//	cout << "The sum of squares of the numbers between " << lower << " and " << upper << " is: " << sum;
+//	return 0;
+//}
