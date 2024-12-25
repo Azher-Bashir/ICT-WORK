0316e3f3dafacae72d435797cd09a15b8af1a7f8
Azher Bashir <azherbashir07@gmail.com>
2024-12-26 00:27:31 +0500

    ICT FINAL PRACTICE QUEASTIONS

---
+++ ICT PRACTICE QUESTIONS/QUESTION 13.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int product = 1, lower, upper;
+//
+//static int pro(int lower_limit, int upper_limit)
+//{
+//	for (int i = lower_limit; i <= upper_limit; i++)
+//	{
+//		product *= i;
+//	}
+//
+//	return product;
+//}
+//
+//int main()
+//{
+//	cout << "Enter the lower limit: ";
+//	cin >> lower;
+//	cout << "Enter the upper limit: ";
+//	cin >> upper;
+//
+//	pro(lower, upper);
+//	 
+//	cout << "Product of the number between " << lower << " and " << upper << " is: " << product;
+//	return 0;
+//}
---
+++ ICT PRACTICE QUESTIONS/QUESTION 15.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int sum = 0, low, up;
+//
+//static int sum_of_squares(int lower, int upper)
+//{
+//	for (int i = lower; i <= upper; i++)
+//	{
+//		if (i % 2 != 0)
+//		{
+//			sum += i;
+//		}
+//		else {
+//			sum = sum;
+//		}
+//	}
+//	return sum;
+//}
+//
+//int main()
+//{
+//	cout << "Enter the lower limit: ";
+//	cin >> low;
+//	cout << "Enter the upper limit: ";
+//	cin >> up;
+//	sum_of_squares(low, up);
+//
+//	cout << "Sum of odd integers between " << low << " and " << up << " is: " << sum;
+//	return 0;
+//
+//}
