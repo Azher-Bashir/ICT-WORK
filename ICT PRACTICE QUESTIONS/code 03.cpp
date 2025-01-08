fb167f2136d61aec1c01818ba78e1be3324ae9e0
Azher-Bashir <azherbashir07@gmail.com>
2025-01-09 00:45:46 +0500

    started learnong oop and some more thongs

---
+++ ICT PRACTICE QUESTIONS/code 03.cpp
@@ -0,0 +1,60 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//static int binary_search(int array[], int n, int num)
+//{
+//    int strt = 0, end = n;
+//
+//    while (strt <= end)
+//    {
+//        int mid = end / 2;
+//
+//        if (array[mid] == num)
+//        {
+//            return mid;
+//        }
+//        else if (array[mid] < num)
+//        {
+//            end = mid + 1;
+//        }
+//        else
+//            strt = mid + 1;
+//    }
+//    return -1;
+//}
+//
+//int main()
+//{
+//    int n;
+//
+//    cout << "Enter the length of the array: ";
+//    cin >> n;
+//
+//    int arr[n];
+//
+//    for (int i = 1; i <= n; i++)
+//    {
+//        cout << "Enter the element at index " << i << ": ";
+//        cin >> arr[i];
+//    }
+//
+//    int num;
+//    cout << "Enter the number of which you want the index: ";
+//    cin >> num;
+//
+//    cout << "The result of binary search is: " << binary_search(arr, n, num) << endl;
+//
+//    return 0;
+//}
+//
+//// #include <iostream>
+//
+//// using namespace std;
+//
+//// int main()
+//// {
+////     cout << "Hello World" << endl;
+//
+////     return 0;
+//// }
---
+++ ICT PRACTICE QUESTIONS/code 2.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//
+//using  namespace std;
+//
+//int main()
+//{
+//	int n;
+//
+//	cout << "Enter the length of the array: ";
+//	cin >> n;
+//
+//	int arr[n];
+//
+//	cout << "Enter the elements of the array: ";
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		cout << "Enter " << i << "th element of the array ";
+//		cin >> arr[i];
+//	}
+//	return 0;
+//}
