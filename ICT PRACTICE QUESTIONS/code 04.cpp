693b4076461db0194b27866462b0bc2a9f15decd
Azher-Bashir <azherbashir07@gmail.com>
2025-01-24 15:48:11 +0500

    starting oop again

--- ICT PRACTICE QUESTIONS/code 04.cpp
+++ ICT PRACTICE QUESTIONS/code 04.cpp
@@ -1,25 +1,25 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	// Array ---> 1) Static Arrray    2) Dynamoc Array
-	const int size1 = 1000;
-	int umair[size1], N;
-	cout << "Enter the size of an Array: ";
-	cin >> N;
-
-	// Input of Array
-	cout << "Enter the elements of the array: ";
-	for (int j = 0; j < N; j++)
-	{
-		cin >> umair[j];
-	}
-
-	// oUTPUT
-	cout << "Array:= ";
-	for (int j = 0; j < size1; j++)
-	{
-		cout << umair[j] << " ";
-	}
-	
-}
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	// Array ---> 1) Static Arrray    2) Dynamoc Array
+//	const int size1 = 1000;
+//	int umair[size1], N;
+//	cout << "Enter the size of an Array: ";
+//	cin >> N;
+//
+//	// Input of Array
+//	cout << "Enter the elements of the array: ";
+//	for (int j = 0; j < N; j++)
+//	{
+//		cin >> umair[j];
+//	}
+//
+//	// oUTPUT
+//	cout << "Array:= ";
+//	for (int j = 0; j < size1; j++)
+//	{
+//		cout << umair[j] << " ";
+//	}
+//	
+//}
