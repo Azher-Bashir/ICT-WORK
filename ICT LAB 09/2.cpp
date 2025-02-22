8657e3d4a7ad037f047876df6f58edd0a26be353
Azher-Bashir <azherbashir07@gmail.com>
2025-02-22 10:54:42 +0500

    Just arranged the uamir lab work

--- ICT LAB 09/2.cpp
+++ ICT LAB 09/2.cpp
@@ -1,41 +1,41 @@
-#include <iostream>
-using namespace std;
-
-class Product {
-private:
-    string name;
-    double price;
-
-public:
-    void setDetails(string n, double p) {
-        name = n;
-        price = p;
-    }
-
-    void display() {
-        cout << "Product Details:" << endl;
-        cout << "Name: " << name << "\nPrice: $" << price << endl;
-    }
-};
-
-int main() {
-    Product product1, product2;
-    string name;
-    double price;
-
-    cout << "Enter product name: ";
-    cin >> name;
-    cout << "Enter product price: ";
-    cin >> price;
-    product1.setDetails(name, price);
-    product1.display();
-
-    cout << "Enter another product name: ";
-    cin >> name;
-    cout << "Enter another product price: ";
-    cin >> price;
-    product2.setDetails(name, price);
-    product2.display();
-
-    return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//class Product {
+//private:
+//    string name;
+//    double price;
+//
+//public:
+//    void setDetails(string n, double p) {
+//        name = n;
+//        price = p;
+//    }
+//
+//    void display() {
+//        cout << "Product Details:" << endl;
+//        cout << "Name: " << name << "\nPrice: $" << price << endl;
+//    }
+//};
+//
+//int main() {
+//    Product product1, product2;
+//    string name;
+//    double price;
+//
+//    cout << "Enter product name: ";
+//    cin >> name;
+//    cout << "Enter product price: ";
+//    cin >> price;
+//    product1.setDetails(name, price);
+//    product1.display();
+//
+//    cout << "Enter another product name: ";
+//    cin >> name;
+//    cout << "Enter another product price: ";
+//    cin >> price;
+//    product2.setDetails(name, price);
+//    product2.display();
+//
+//    return 0;
+//}
--- ICT LAB 09/5.cpp
+++ ICT LAB 09/5.cpp
@@ -1,76 +1,76 @@
-#include <iostream>
-using namespace std;
-
-class Student {
-private:
-    char name[50];
-    int rollNo;
-    int marks[5];
-
-public:
-    // Default Constructor
-    Student() {
-        rollNo = 0;
-        name[0] = '\0';
-        for (int i = 0; i < 5; i++) {
-            marks[i] = 0;
-        }
-    }
-
-    // Parameterized Constructor
-    Student(const char n[], int r, int m[]) {
-        strcpy(name, n);
-        rollNo = r;
-        for (int i = 0; i < 5; i++) {
-            marks[i] = m[i];
-        }
-    }
-
-    void setDetails(const char n[], int r, int m[]) {
-        strcpy(name, n);
-        rollNo = r;
-        for (int i = 0; i < 5; i++) {
-            marks[i] = m[i];
-        }
-    }
-
-    int getTotalMarks() {
-        int total = 0;
-        for (int i = 0; i < 5; i++) {
-            total += marks[i];
-        }
-        return total;
-    }
-
-    double getAverageMarks() {
-        return getTotalMarks() / 5.0;
-    }
-
-    void displayDetails() {
-        cout << "Name: " << name << endl;
-        cout << "Roll Number: " << rollNo << endl;
-        cout << "Total Marks: " << getTotalMarks() << endl;
-        cout << "Average Marks: " << getAverageMarks() << endl;
-    }
-};
-
-int main() {
-    Student student1;
-    char name[50];
-    int rollNo, marks[5];
-
-    cout << "Enter Student Name: ";
-    cin.getline(name, 50);
-    cout << "Enter Roll Number: ";
-    cin >> rollNo;
-    cout << "Enter Marks for 5 subjects: ";
-    for (int i = 0; i < 5; i++) {
-        cin >> marks[i];
-    }
-    cin.ignore();
-
-    student1.setDetails(name, rollNo, marks);
-    student1.displayDetails();
-
-    return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//class Student {
+//private:
+//    char name[50];
+//    int rollNo;
+//    int marks[5];
+//
+//public:
+//    // Default Constructor
+//    Student() {
+//        rollNo = 0;
+//        name[0] = '\0';
+//        for (int i = 0; i < 5; i++) {
+//            marks[i] = 0;
+//        }
+//    }
+//
+//    // Parameterized Constructor
+//    Student(const char n[], int r, int m[]) {
+//        strcpy(name, n);
+//        rollNo = r;
+//        for (int i = 0; i < 5; i++) {
+//            marks[i] = m[i];
+//        }
+//    }
+//
+//    void setDetails(const char n[], int r, int m[]) {
+//        strcpy(name, n);
+//        rollNo = r;
+//        for (int i = 0; i < 5; i++) {
+//            marks[i] = m[i];
+//        }
+//    }
+//
+//    int getTotalMarks() {
+//        int total = 0;
+//        for (int i = 0; i < 5; i++) {
+//            total += marks[i];
+//        }
+//        return total;
+//    }
+//
+//    double getAverageMarks() {
+//        return getTotalMarks() / 5.0;
+//    }
+//
+//    void displayDetails() {
+//        cout << "Name: " << name << endl;
+//        cout << "Roll Number: " << rollNo << endl;
+//        cout << "Total Marks: " << getTotalMarks() << endl;
+//        cout << "Average Marks: " << getAverageMarks() << endl;
+//    }
+//};
+//
+//int main() {
+//    Student student1;
+//    char name[50];
+//    int rollNo, marks[5];
+//
+//    cout << "Enter Student Name: ";
+//    cin.getline(name, 50);
+//    cout << "Enter Roll Number: ";
+//    cin >> rollNo;
+//    cout << "Enter Marks for 5 subjects: ";
+//    for (int i = 0; i < 5; i++) {
+//        cin >> marks[i];
+//    }
+//    cin.ignore();
+//
+//    student1.setDetails(name, rollNo, marks);
+//    student1.displayDetails();
+//
+//    return 0;
+//}
