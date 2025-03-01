ac2a32670b0150e77fa530aeafb76dec440b8af3
Azher-Bashir <azherbashir07@gmail.com>
2025-03-01 22:53:25 +0500

    working on assignment

---
+++ ICT ASSIGNMENTS/Question # 03.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+bool identical(int* array1, int* array2, int size1, int size2)
+{
+	if (size1 != size2)
+	{
+		return false;
+	}
+	else
+	{
+		for (int i = 0; i < size1; i++)
+		{
+			if (array1[i] != array2[i])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+
+int main()
+{
+	int size1, size2;
+	cout << "Ent erhte size of first array: ";
+	cin >> size1;
+	cout << "Ente the size of the second array: ";
+	cin >> size2;
+
+	int* array1 = new int[size1]; 
+	int* array2 = new int[size2];
+
+	cout << "Enter the elemments of the first arrary: " << endl;
+	for (int i = 0; i < size1; i++)
+	{
+		cout << "Enter the " << i << "th element of the first array: ";
+		cin >> array1[i];
+	}
+	
+	cout << "Enter the elemments of the Second arrary: " << endl;
+	for (int i = 0; i < size2; i++)
+	{
+		cout << "Enter the " << i << "th element of the Second array: ";
+		cin >> array2[i];
+	}
+	//identical(array1, array2, size1, size2);
+	cout << "The arrays are identical: " << identical(array1, array2, size1, size2) << endl;
+}
