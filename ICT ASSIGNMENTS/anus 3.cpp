657f29a6067972cef238391f2e7ce4b0ea09aae6
Azher-Bashir <azherbashir07@gmail.com>
2025-03-04 19:49:41 +0500

    3 codes done of anus ahmed

--- ICT ASSIGNMENTS/anus 3.cpp
+++ ICT ASSIGNMENTS/anus 3.cpp
@@ -1,48 +1,49 @@
-//You are required to write a program which will take input from user in two integer arrays.The
-//program should compare both arrays for checking if both arrays are totally identical(Exactly same in size
-//	and elements).
-//	Write a boolean function which return true if arrays are totally identical otherwise return false
-
-#include <iostream>
-using namespace std;
-
-bool identical(int* array1, int* array2, int size) {
-	for (int i = 0; i < size; i++) {
-		if (array1[i] != array2[i]) {
-			return false;
-		}
-	}
-	return true;
-}
-
-int main() {
-	int size1, size2;
-	cout << "Enter the size of the first array: ";
-	cin >> size1;
-	cout << "Enter the size of the second array: ";
-	cin >> size2;
-	if (size1 != size2) {
-		cout << "The arrays are not equal in size." << endl;
-		return 0;
-	}
-	int* array1 = new int[size1];
-	int* array2 = new int[size2];
-	cout << "Enter the elements of the first array:" << endl;
-	for (int i = 0; i < size1; i++) {
-		cin >> array1[i];
-	}
-	cout << "Enter the elements of the second array:" << endl;
-	for (int i = 0; i < size2; i++) {
-		cin >> array2[i];
-	}
-	bool result = identical(array1, array2, size1);
-	if (result) {
-		cout << "The entered arrays are equal to each other in size and entries." << endl;
-	}
-	else {
-		cout << "The entered arrays are not equal to each other in size and entries." << endl;
-	}
-	delete[] array1;
-	delete[] array2;
-	return 0;
-}
+////You are required to write a program which will take input from user in two integer arrays.The
+////program should compare both arrays for checking if both arrays are totally identical(Exactly same in size
+////	and elements).
+////	Write a boolean function which return true if arrays are totally identical otherwise return false
+//
+//#include <iostream>
+//using namespace std;
+//
+//bool are_arrays_identical(int* arr1, int* arr2, int length) {
+//    for (int i = 0; i < length; i++) {
+//        if (arr1[i] != arr2[i]) {
+//            return false;
+//        }
+//    }
+//    return true;
+//}
+//
+//int main() {
+//    int length1, length2;
+//    cout << "Enter the size of the first array: ";
+//    cin >> length1;
+//    cout << "Enter the size of the second array: ";
+//    cin >> length2;
+//    if (length1 != length2) {
+//        cout << "The arrays are not equal in size." << endl;
+//        return 0;
+//    }
+//    int* arr1 = new int[length1];
+//    int* arr2 = new int[length2];
+//    cout << "Enter the elements of the first array:" << endl;
+//    for (int i = 0; i < length1; i++) {
+//        cin >> arr1[i];
+//    }
+//    cout << "Enter the elements of the second array:" << endl;
+//    for (int i = 0; i < length2; i++) {
+//        cin >> arr2[i];
+//    }
+//    bool are_identical = are_arrays_identical(arr1, arr2, length1);
+//    if (are_identical) {
+//        cout << "The entered arrays are identical in size and elements." << endl;
+//    }
+//    else {
+//        cout << "The entered arrays are not identical in size and elements." << endl;
+//    }
+//    delete[] arr1;
+//    delete[] arr2;
+//    return 0;
+//}
+//
