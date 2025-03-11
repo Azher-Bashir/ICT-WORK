f752b8bcc51b0999da13c2c60efedb1f32c66a9f
Azher-Bashir <azherbashir07@gmail.com>
2025-03-11 23:29:01 +0500

    RAFAY ALI KHAN ASSIGNMENT DONE COMPLETLY

--- ICT PRACTICE QUESTIONS/code005.cpp
+++ ICT PRACTICE QUESTIONS/code005.cpp
@@ -6,27 +6,45 @@ int main()
 {
     // Open the file to read the three words
     ifstream file("sentence.txt");
+    ofstream tempFile("temp.txt");
     char ch;
 
     // Read and print the first word
     while (file.get(ch) && ch != ' ') {
         cout.put(ch);
+        tempFile.put(ch);
     }
     cout.put(' ');
+    tempFile.put(' ');
 
     // Read and print the second word
     while (file.get(ch) && ch != ' ') {
         cout.put(ch);
+        tempFile.put(ch);
     }
     cout.put(' ');
+    tempFile.put(' ');
 
     // Read and print the third word
     while (file.get(ch) && ch != '\n' && file) {
         cout.put(ch);
+        tempFile.put(ch);
     }
     cout.put('\n');
+    tempFile.put('\n');
 
     file.close();
+    tempFile.close();
+
+    // Write the content from the temporary file back to the original file
+    ifstream tempFileRead("temp.txt");
+    ofstream outFile("sentence.txt");
+    while (tempFileRead.get(ch)) {
+        outFile.put(ch);
+    }
+    tempFileRead.close();
+    outFile.close();
 
     return 0;
 }
+
