29ff17d840c37508d2a1ad04224b714f66610204
Azher-Bashir <azherbashir07@gmail.com>
2025-03-13 16:26:07 +0500

    lab 09 doing it today submission left

---
+++ ICT LAB 09/L6 T1 (Dry run).cpp
@@ -0,0 +1,43 @@
+//// Example program
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//class student
+//{
+//private:
+//	string name;
+//	int rollnumber;
+//public:
+//	student();//default constructor
+//	student(string, int);//parametrized constructor
+//
+//
+//	void function();
+//};
+//
+//
+//student::student(string name, int age)
+//{
+//	name = " ";
+//	rollnumber = 0;
+//	cout << "Default constructor called" << endl;
+//}
+//
+//
+//
+//void student::function()
+//{
+//
+//}
+//
+//
+//int main()
+//{
+//	student A;
+//
+//	student b("Azher", 12);
+//
+//	student* ptrA = new student();
+//	delete ptrA;
+//	return 0;
+//}
