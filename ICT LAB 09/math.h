500ced835a3e1c36b38418777bc7cf5e0322198e
Azher-Bashir <azherbashir07@gmail.com>
2025-04-14 23:25:40 +0500

    just commented the open ended lab anf did some addition in practice quwstions

--- ICT LAB 09/math.h
+++ ICT LAB 09/math.h
@@ -1,16 +1,16 @@
-#ifndef MATH_H
-#define MATH_H
-
-#include "student.h"
-
-class math_marks : public student {
-    double quiz[3], test[2], final;
-public:
-    math_marks(string, string, double, double, double, double, double, double);
-    double Math_grade() const;
-    double get_grade() const override {
-        return Math_grade();
-    }
-};
-
-#endif // MATH_H
+//#ifndef MATH_H
+//#define MATH_H
+//
+//#include "student.h"
+//
+//class math_marks : public student {
+//    double quiz[3], test[2], final;
+//public:
+//    math_marks(string, string, double, double, double, double, double, double);
+//    double Math_grade() const;
+//    double get_grade() const override {
+//        return Math_grade();
+//    }
+//};
+//
+//#endif // MATH_H
