e1fd8dc53888cbfa95ee25a772a4a6c21c536ad5
Azher-Bashir <azherbashir07@gmail.com>
2025-04-30 23:05:05 +0500

    wokring on assignment number 3

--- ICT ASSIGNMENTS/Show room.cpp
+++ ICT ASSIGNMENTS/Show room.cpp
@@ -1,29 +1,56 @@
-//#include<iostream>
-//using namespace std;
-//template<class T1>
-//class add
-//{
-//private:
-//	T1 num1;
-//	T1 num2;
-//public:
-//	add(T1, T1);
-//	T1 add_num();
-//};
-//template <class T1>
-//add<T1>::add(T1 a, T1 b) : num1(a), num2(b)
-//{}
-//template <class T1>
-//T1 add<T1>::add_num()
-//{
-//	return num1 + num2;
-//}
-//
-//int main()
-//{
-//	add<int>a(2, 3);
-//	add<double>b(2.4323244, 33.323234);
-//	auto sum_int = a.add_num();
-//	auto sum_double = b.add_num();
-//	cout << "Sum of integers: " << sum_int << endl << "Sum of double: " << sum_double << endl;
-//}
+#include <iostream>]
+using namespace std;
+
+class vehical
+{
+private:
+	int size_available;
+	int size_not_avail;
+	string* vehicals_available;
+	string* vehicals_not_available;
+public:
+	vehical();
+	vehical(string*, string*, int, int);
+	void set_available(string*);
+	void set_not_available(string*);
+	void set_no_of_cars(int, int);
+	string* get_available();
+	string* get_not_available();
+	string get_no_of_cars();
+};
+vehical::vehical()
+{
+	size_available = 0;
+	size_not_avail = 0;
+	vehicals_available = nullptr;
+	vehicals_not_available = nullptr;
+}
+vehical::vehical(string* available, string* not_available, int size_avail, int size_not_avail)
+{
+	vehicals_available = available;
+	vehicals_not_available = not_available;
+	size_available = size_avail;
+	this->size_not_avail = size_not_avail;
+}
+void vehical::set_available(string* available)
+{
+	vehicals_available = available;
+}
+void vehical::set_not_available(string* not_available)
+{
+	vehicals_not_available = not_available;
+}
+void vehical::set_no_of_cars(int available, int not_available)
+{
+	size_available = available;
+	size_not_avail = not_available;
+}
+string* vehical::get_available()
+{
+	return vehicals_available;
+}
+string* vehical::get_not_available()
+{
+	return vehicals_not_available;
+}
+string 
