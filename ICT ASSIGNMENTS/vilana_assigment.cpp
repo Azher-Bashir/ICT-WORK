32133416356f2b5ffb24ac3ecb5f85d99902112f
Azher Bashir <azherbashir07@gmail.com>
2025-05-03 23:42:15 +0500

    assignment 3 done

--- ICT ASSIGNMENTS/vilana_assigment.cpp
+++ ICT ASSIGNMENTS/vilana_assigment.cpp
@@ -98,9 +98,12 @@ class Date {
 public:
     Date() : day(0), month(0), year(0) {}
     void Set_date() {
-        cout << "Day: "; cin >> day;
-        cout << "Month: "; cin >> month;
-        cout << "Year: "; cin >> year;
+        cout << "Day: ";
+        cin >> day;
+        cout << "Month: ";
+        cin >> month;
+        cout << "Year: ";
+        cin >> year;
     }
     void Display() const {
         cout << day << "/" << month << "/" << year;
@@ -134,6 +137,7 @@ public:
         cout << "From: "; start.Display(); cout << " to "; end.Display(); cout << endl;
         cout << "Total Cost: " << total << "\n";
         cout << "--------------------------------------\n";
+        cout << endl;
     }
     bool isCustomer(long long cnic) const {
         return customer->get_cnic() == cnic;
@@ -145,26 +149,95 @@ int main() {
     list<Customer*> customers;
     list<Vehicles*> vehicles;
     list<Rental_Transaction*> rentals;
+
+    // Adding cars and trucks using pointers to arrays
+    Car* car[5];
+    car[0] = new Car();
+    car[0]->set_model("Toyota Corolla");
+    car[0]->set_year(2020); car[0]->set_rent(50);
+    car[0]->set_status(true);
+
+    car[1] = new Car();
+    car[1]->set_model("Honda Civic");
+    car[1]->set_year(2019); car[1]->set_rent(60);
+    car[1]->set_status(false);
+
+    car[2] = new Car();
+    car[2]->set_model("Hyundai Elantra");
+    car[2]->set_year(2021); car[2]->set_rent(55); 
+    car[2]->set_status(true);
+
+    car[3] = new Car();
+    car[3]->set_model("Kia Sportage");
+    car[3]->set_year(2022); car[3]->set_rent(70);
+    car[3]->set_status(true);
+
+    car[4] = new Car();
+    car[4]->set_model("Nissan Altima"); 
+    car[4]->set_year(2018); car[4]->set_rent(45);
+    car[4]->set_status(false);
+
+    Truck* truck[5];
+    truck[0] = new Truck();
+    truck[0]->set_model("Ford F-150");
+    truck[0]->set_year(2021);
+    truck[0]->set_rent(100);
+    truck[0]->set_status(true);
+
+    truck[1] = new Truck();
+    truck[1]->set_model("Chevrolet Silverado");
+    truck[1]->set_year(2020);
+    truck[1]->set_rent(110);
+    truck[1]->set_status(false);
+
+    truck[2] = new Truck();
+    truck[2]->set_model("Ram 1500");
+    truck[2]->set_year(2022);
+    truck[2]->set_rent(120);
+    truck[2]->set_status(true);
+
+    truck[3] = new Truck();
+    truck[3]->set_model("GMC Sierra");
+    truck[3]->set_year(2019);
+    truck[3]->set_rent(105);
+    truck[3]->set_status(true);
+
+    truck[4] = new Truck();
+    truck[4]->set_model("Toyota Tundra");
+    truck[4]->set_year(2018);
+    truck[4]->set_rent(95); 
+    truck[4]->set_status(false);
+
+
+    // Adding vehicles to the list
+    for (int i = 0; i < 5; i++) {
+        vehicles.Add_item(car[i]);
+        vehicles.Add_item(truck[i]);
+    }
+
     int choice = 0;
     while (true) {
         cout <<
-            "========================================";
+            "_-=-_-=-_-=-_-=-_-=-_-=-_-=-_-=-_-=-_-=-";
         cout << "      WELCOME TO VEHICLE RENTAL SYSTEM      ";
-        cout << "========================================";
+        cout << "_-=-_-=-_-=-_-=-_-=-_-=-_-=-_-=-_-=-_-=-";
         cout << "1. Add Customer";
         cout << "2. Add Vehicle";
         cout << "3. Book Vehicle";
         cout << "4. Search Rental History";
         cout << "5. Show Available Vehicles";
+        cout << "6. Show Not Available Vehicles";
         cout << "9. Exit";
-        cout << "========================================";
+        cout << "_-=-_-=-_-=-_-=-_-=-_-=-_-=-_-=-_-=-_-=-";
         cout << "Enter your choice: ";
-        cout << "\n1. Add Customer\n2. Add Vehicle\n3. Book Vehicle\n4. Search Rental History\n5. Show Available Vehicles\n9. Exit\nChoose: ";
+        cout << "\n1. Add Customer\n2. Add Vehicle\n3. Book Vehicle\n4. Search Rental History\n5. Show Available Vehicles\n6. Show Not Available Vehicles\n9. Exit\nChoose: ";
         cin >> choice;
+        cout << endl;
         if (choice == 1) {
             Customer* c = new Customer;
             c->Set_Customer_detail();
             customers.Add_item(c);
+            cout << endl;
         }
         else if (choice == 2) {
             int t;
@@ -179,6 +252,7 @@ int main() {
             else v = new Truck();
             v->set_model(m); v->set_year(y); v->set_rent(r); v->set_status(true);
             vehicles.Add_item(v);
+            cout << endl;
         }
         else if (choice == 3) {
             long long cnic; string model;
@@ -210,6 +284,8 @@ int main() {
             else {
                 cout << "Customer or Vehicle not found or unavailable.\n";
             }
+            cout << endl;
+
         }
         else if (choice == 4) {
             long long cnic;
@@ -218,6 +294,8 @@ int main() {
                 if (rentals.get_Data(i)->isCustomer(cnic))
                     rentals.get_Data(i)->display();
             }
+            cout << endl;
+
         }
         else if (choice == 5) {
             cout << "Available Vehicles:\n";
@@ -226,14 +304,29 @@ int main() {
                     vehicles.get_Data(i)->displayDetails();
                 }
             }
+            cout << endl;
+
+        }
+        else if (choice == 6) { // New option for not available vehicles
+            cout << "Not Available Vehicles:\n";
+            for (int i = 0; i < vehicles.get_size(); i++) {
+                if (!vehicles.get_Data(i)->get_status()) {
+                    vehicles.get_Data(i)->displayDetails();
+                }
+            }
+            cout << endl;
+
         }
         else if (choice == 9) {
             cout << "Exiting program.\n";
+            cout << endl;
             break;
         }
         else {
             cout << "Invalid option. Try again.\n";
+            cout << endl;
         }
+
     }
     return 0;
 }
