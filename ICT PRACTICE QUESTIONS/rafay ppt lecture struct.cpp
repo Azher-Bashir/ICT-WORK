0144b26984c5bc57871544ad7b31e4c2742040be
Azher-Bashir <azherbashir07@gmail.com>
2025-07-18 19:19:51 +0500

    committed from git desktop.

---
+++ ICT PRACTICE QUESTIONS/rafay ppt lecture struct.cpp
@@ -0,0 +1,52 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() 
+//{
+//struct Student {
+//	string name;
+//	int rollNo = 0; 
+//	float marks = 0.0f; 
+//};
+//struct teacher {
+//	string name;
+//	int id = 0; 
+//	float salary = 0.0f;
+//};
+//Student student1;
+//cout << "Enter student's name: ";
+//cin >> student1.name;
+//cout << "Enter student's roll number: ";
+//cin >> student1.rollNo;
+//cout << "Enter student's marks: ";
+//cin >> student1.marks;
+//cout << "\nStudent Details:\n";
+//cout << "Name: " << student1.name << endl;
+//cout << "Roll Number: " << student1.rollNo << endl;
+//cout << "Marks: " << student1.marks << endl;
+//Student student2;
+//cout << "\nEnter second student's name: ";
+//cin >> student2.name;
+//cout << "Enter second student's roll number: ";
+//cin >> student2.rollNo;
+//cout << "Enter second student's marks: ";
+//cin >> student2.marks;
+//cout << "\nSecond Student Details:\n";
+//cout << "Name: " << student2.name << endl;
+//cout << "Roll Number: " << student2.rollNo << endl;
+//cout << "Marks: " << student2.marks << endl;
+//teacher teacher1;
+//cout << "\nEnter teacher's name: ";
+//cin >> teacher1.name;
+//cout << "Enter teacher's ID: ";
+//cin >> teacher1.id;
+//cout << "Enter teacher's salary: ";
+//cin >> teacher1.salary;
+//cout << "\nTeacher Details:\n";
+//cout << "Name: " << teacher1.name << endl;
+//cout << "ID: " << teacher1.id << endl;
+//cout << "Salary: " << teacher1.salary << endl;
+//
+//return 0;
+//}dd
