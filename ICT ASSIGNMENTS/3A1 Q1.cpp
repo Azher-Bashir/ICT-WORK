6c083d036040dc517f5f1c7a7736e220d636eee6
Azher Bashir <azherbashir07@gmail.com>
2025-09-15 16:57:38 +0500

    asssignment number 2 last question (2) done

--- ICT ASSIGNMENTS/3A1 Q1.cpp
+++ ICT ASSIGNMENTS/3A1 Q1.cpp
@@ -1,76 +1,82 @@
-#include <iostream>
-using namespace std;
-class Node {
-	private:
-	int data;
-	Node* next;
-public:
-	Node(int val) {
-		data = val;
-		next = nullptr;
-	}
-	friend class LinkedList;
-};
-class LinkedList {
-	private:
-		Node* head;
-public:
-	LinkedList() {
-		head = nullptr;
-	}
-	void insert(int val);
-	void split(LinkedList& evenList, LinkedList& oddList);
-	void print();
-};
-
-void LinkedList::insert(int val) {
-	Node* newNode = new Node(val);
-	if (!head) {
-		head = newNode;
-	} else {
-		Node* temp = head;
-		while (temp->next) {
-			temp = temp->next;
-		}
-		temp->next = newNode;
-	}
-}
-void LinkedList::split(LinkedList& evenList, LinkedList& oddList) {
-	Node* temp = head;
-	int index = 0;
-	while (temp) {
-		if (index % 2 == 0) {
-			evenList.insert(temp->data);
-		} else {
-			oddList.insert(temp->data);
-		}
-		temp = temp->next;
-		index++;
-	}
-}
-void LinkedList::print() {
-	Node* temp = head;
-	while (temp) {
-		cout << temp->data << " ";
-		temp = temp->next;
-	}
-	cout << endl;
-}
-int main() {
-	LinkedList list;
-	int n, val;
-	cout << "Enter number of elements: ";
-	cin >> n;
-	cout << "Enter elements: ";
-	for (int i = 0; i < n; i++) {
-		cin >> val;
-		list.insert(val);
-	}
-	LinkedList evenList, oddList;
-	list.split(evenList, oddList);
-	cout << "Even position list: ";
-	evenList.print();
-	cout << "Odd position list: ";
-	oddList.print();
-	return 0;
-}
+////You are given a singly linked list of integers.Write the code to split it into two new linked lists :
+////• List A : contains nodes at even positions(0 - based indexing).
+////• List B : contains nodes at odd positions.
+////Finally, print both lists.
+////write code in c++ without using vectors and use classes in which make the variable private and make the defination of the functions outside the class.
+//
+//#include <iostream>
+//using namespace std;
+//class Node {
+//	private:
+//	int data;
+//	Node* next;
+//public:
+//	Node(int val) {
+//		data = val;
+//		next = nullptr;
+//	}
+//	friend class LinkedList;
+//};
+//class LinkedList {
+//	private:
+//		Node* head;
+//public:
+//	LinkedList() {
+//		head = nullptr;
+//	}
+//	void insert(int val);
+//	void split(LinkedList& evenList, LinkedList& oddList);
+//	void print();
+//};
+//
+//void LinkedList::insert(int val) {
+//	Node* newNode = new Node(val);
+//	if (!head) {
+//		head = newNode;
+//	} else {
+//		Node* temp = head;
+//		while (temp->next) {
+//			temp = temp->next;
+//		}
+//		temp->next = newNode;
+//	}
+//}
+//void LinkedList::split(LinkedList& evenList, LinkedList& oddList) {
+//	Node* temp = head;
+//	int index = 0;
+//	while (temp) {
+//		if (index % 2 == 0) {
+//			evenList.insert(temp->data);
+//		} else {
+//			oddList.insert(temp->data);
+//		}
+//		temp = temp->next;
+//		index++;
+//	}
+//}
+//void LinkedList::print() {
+//	Node* temp = head;
+//	while (temp) {
+//		cout << temp->data << " ";
+//		temp = temp->next;
+//	}
+//	cout << endl;
+//}
+//int main() {
+//	LinkedList list;
+//	int n, val;
+//	cout << "Enter number of elements: ";
+//	cin >> n;
+//	cout << "Enter elements: ";
+//	for (int i = 0; i < n; i++) {
+//		cin >> val;
+//		list.insert(val);
+//	}
+//	LinkedList evenList, oddList;
+//	list.split(evenList, oddList);
+//	cout << "Even position list: ";
+//	evenList.print();
+//	cout << "Odd position list: ";
+//	oddList.print();
+//	return 0;
+//}
