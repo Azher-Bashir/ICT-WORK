c100bb1afc255fe6ca6beb8fd1609ddcea17b52d
Azher Bashir <azherbashir07@gmail.com>
2025-09-15 16:53:36 +0500

    question number 1 done of the assignment number 1 of 3rd semester

--- ICT ASSIGNMENTS/3A1 Q1.cpp
+++ ICT ASSIGNMENTS/3A1 Q1.cpp
@@ -1,38 +1,76 @@
-//#include <iostream>
-//using namespace std;
-//
-//static int elementPosition(int dimensions, int* index, int* dimensionSize) {
-//    int position = 0;
-//    int multiplier = 1;
-//    for (int i = dimensions - 1; i >= 0; i--) {
-//        position += index[i] * multiplier;
-//        multiplier *= dimensionSize[i];
-//    }
-//    return position;
-//}
-//
-//int main() {
-//    int dimensions;
-//    int* dimensionSize = new int[dimensions];
-//    int* index = new int[dimensions];
-//    cout << "Enter number of dimensions: ";
-//    cin >> dimensions;
-//    cout << "Enter size of each dimension:\n";
-//    for (int i = 0; i < dimensions; i++)
-//    {
-//        cout << "Size of dimension " << i << ": ";
-//        cin >> dimensionSize[i];
-//    }
-//    cout << "Enter index of element in each dimension:\n";
-//    for (int i = 0; i < dimensions; i++) 
-//    {
-//        cout << "Index in dimension " << i << ": ";
-//        cin >> index[i];
-//    }
-//    int pos = elementPosition(dimensions, index, dimensionSize);
-//    cout << "\nLinear position of the element in row-major order = " << pos << endl;
-//
-//    delete[] dimensionSize;
-//    delete[] index;
-//    return 0;
-//}
+#include <iostream>
+using namespace std;
+class Node {
+	private:
+	int data;
+	Node* next;
+public:
+	Node(int val) {
+		data = val;
+		next = nullptr;
+	}
+	friend class LinkedList;
+};
+class LinkedList {
+	private:
+		Node* head;
+public:
+	LinkedList() {
+		head = nullptr;
+	}
+	void insert(int val);
+	void split(LinkedList& evenList, LinkedList& oddList);
+	void print();
+};
+
+void LinkedList::insert(int val) {
+	Node* newNode = new Node(val);
+	if (!head) {
+		head = newNode;
+	} else {
+		Node* temp = head;
+		while (temp->next) {
+			temp = temp->next;
+		}
+		temp->next = newNode;
+	}
+}
+void LinkedList::split(LinkedList& evenList, LinkedList& oddList) {
+	Node* temp = head;
+	int index = 0;
+	while (temp) {
+		if (index % 2 == 0) {
+			evenList.insert(temp->data);
+		} else {
+			oddList.insert(temp->data);
+		}
+		temp = temp->next;
+		index++;
+	}
+}
+void LinkedList::print() {
+	Node* temp = head;
+	while (temp) {
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+int main() {
+	LinkedList list;
+	int n, val;
+	cout << "Enter number of elements: ";
+	cin >> n;
+	cout << "Enter elements: ";
+	for (int i = 0; i < n; i++) {
+		cin >> val;
+		list.insert(val);
+	}
+	LinkedList evenList, oddList;
+	list.split(evenList, oddList);
+	cout << "Even position list: ";
+	evenList.print();
+	cout << "Odd position list: ";
+	oddList.print();
+	return 0;
+}
