3a308359f8f05e5a8261ea05a7ea498ab6e1685d
Azher Bashir <azherbashir07@gmail.com>
2025-09-16 20:58:48 +0500

    assignmetn 1 done and submitted

---
+++ ICT ASSIGNMENTS/3A1 Q2(main).cpp
@@ -0,0 +1,17 @@
+//#include "3A1 Q2.h"
+//
+//int main() {
+//    Hospital hospital;
+//
+//    hospital.admit(1, "Azher Bashir", "Flu");
+//    hospital.admit(2, "Hussnain", "Cold");
+//    hospital.admit(3, "Villana", "Fever");
+//
+//    hospital.print();
+//
+//    hospital.discharge(2);
+//    hospital.print();
+//
+//    hospital.discharge(4); // not in the memory or you can say invalide
+//    return 0;
+//}
--- ICT ASSIGNMENTS/3A1 Q2.h
+++ ICT ASSIGNMENTS/3A1 Q2.h
@@ -1,97 +1,90 @@
-#ifndef HOSPITAL_H
-#define HOSPITAL_H
-
-#include <iostream>
-#include <string>
-using namespace std;
-
-// ================= PATIENT CLASS =================
-class Patient {
-private:
-    int id;
-    string name;
-    string disease;
-    Patient* next;
-
-public:
-    Patient(int pid, string pname, string pdisease) {
-        id = pid;
-        name = pname;
-        disease = pdisease;
-        next = nullptr;
-    }
-
-    friend class Hospital;
-};
-
-// ================= HOSPITAL CLASS =================
-class Hospital {
-private:
-    Patient* head;
-
-public:
-    Hospital() {
-        head = nullptr;
-    }
-
-    // Admit a new patient
-    void admit(int pid, string pname, string pdisease) {
-        Patient* newPatient = new Patient(pid, pname, pdisease);
-        if (!head) {
-            head = newPatient;
-        }
-        else {
-            Patient* temp = head;
-            while (temp->next) {
-                temp = temp->next;
-            }
-            temp->next = newPatient;
-        }
-    }
-
-    // Discharge a patient by ID
-    void discharge(int pid) {
-        if (!head) {
-            cout << "No patients to discharge." << endl;
-            return;
-        }
-        if (head->id == pid) {
-            Patient* temp = head;
-            head = head->next;
-            delete temp;
-            cout << "Patient with ID " << pid << " discharged." << endl;
-            return;
-        }
-        Patient* temp = head;
-        while (temp->next && temp->next->id != pid) {
-            temp = temp->next;
-        }
-        if (temp->next) {
-            Patient* toDelete = temp->next;
-            temp->next = temp->next->next;
-            delete toDelete;
-            cout << "Patient with ID " << pid << " discharged." << endl;
-        }
-        else {
-            cout << "Patient with ID " << pid << " not found." << endl;
-        }
-    }
-
-    // Print all admitted patients
-    void print() {
-        if (!head) {
-            cout << "No patients admitted." << endl;
-            return;
-        }
-        Patient* temp = head;
-        cout << "Currently admitted patients:" << endl;
-        while (temp) {
-            cout << "ID: " << temp->id
-                << ", Name: " << temp->name
-                << ", Disease: " << temp->disease << endl;
-            temp = temp->next;
-        }
-    }
-};
-
-#endif
+//#ifndef HOSPITAL_H
+//#define HOSPITAL_H
+//
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//class Patient {
+//private:
+//    int id;
+//    string name;
+//    string disease;
+//    Patient* next;
+//
+//public:
+//    Patient(int pid, string pname, string pdisease) {
+//        id = pid;
+//        name = pname;
+//        disease = pdisease;
+//        next = nullptr;
+//    }
+//
+//    friend class Hospital;
+//};
+//class Hospital {
+//private:
+//    Patient* head;
+//
+//public:
+//    Hospital() {
+//        head = nullptr;
+//    }
+//
+//    // Admit a new patient
+//    void admit(int pid, string pname, string pdisease) {
+//        Patient* newPatient = new Patient(pid, pname, pdisease);
+//        if (!head) {
+//            head = newPatient;
+//        }
+//        else {
+//            Patient* temp = head;
+//            while (temp->next) {
+//                temp = temp->next;
+//            }
+//            temp->next = newPatient;
+//        }
+//    }
+//    void discharge(int pid) {
+//        if (!head) {
+//            cout << "No patients to discharge." << endl;
+//            return;
+//        }
+//        if (head->id == pid) {
+//            Patient* temp = head;
+//            head = head->next;
+//            delete temp;
+//            cout << "Patient with ID " << pid << " discharged." << endl;
+//            return;
+//        }
+//        Patient* temp = head;
+//        while (temp->next && temp->next->id != pid) {
+//            temp = temp->next;
+//        }
+//        if (temp->next) {
+//            Patient* toDelete = temp->next;
+//            temp->next = temp->next->next;
+//            delete toDelete;
+//            cout << "Patient with ID " << pid << " discharged." << endl;
+//        }
+//        else {
+//            cout << "Patient with ID " << pid << " not found." << endl;
+//        }
+//    }
+//    void print() {
+//        if (!head) {
+//            cout << "No patients admitted." << endl;
+//            return;
+//        }
+//        Patient* temp = head;
+//        cout << "Currently admitted patients:" << endl;
+//        while (temp) {
+//            cout << "ID: " << temp->id
+//                << ", Name: " << temp->name
+//                << ", Disease: " << temp->disease << endl;
+//            temp = temp->next;
+//        }
+//    }
+//};
+//
+//#endif
