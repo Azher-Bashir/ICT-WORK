ef067962fe4e6cfcc2ad5c8cd65c18e2b116ad6d
Azher_Bashir <Azher-Bashir@users.noreply.github.com>
2025-11-29 01:19:32 +0500

    OODS ASSINGMENT ALMOST DONE

--- ICT ASSIGNMENTS/Shipment.h
+++ ICT ASSIGNMENTS/Shipment.h
@@ -10,20 +10,26 @@ public:
 	int id;
 	string sender;
 	string receiver;
-	string FomCity;
+	string FromCity;
 	string ToCity;
+	Shipment() : id(-1), sender(""), receiver(""), FromCity(""), ToCity("") {}
+	Shipment(int id, string s, string r, string fc, string tc) : id(id), sender(s), receiver(r), FromCity(fc), ToCity(tc) {}
 };
 class HashTable {
 private:
 	int SIZE;
 	Shipment* table;
-	bool occupied = new bool [SIZE];
+	int* occupied;
 public:
 	HashTable(int hashsize)
 	{
 		SIZE = hashsize;
 		table = new Shipment[SIZE];
-		occupied = NULL;
+		occupied = new int[SIZE];
+		for (int i = 0; i < SIZE; i++)
+		{
+			occupied[i] = 0;
+		}
 	}
 	int hashFunction(int key)
 	{
@@ -31,14 +37,68 @@ public:
 	}
 	void insertShipment(int id, string s, string r, string fromcity, string tocity)
 	{
+		Shipment newshipment(id, s, r, fromcity, tocity);
 		int index = hashFunction(id);
 		int start = index;
-		if (table[index] == id) {
-
-		}
+		do {
+			if (occupied[index] == 0 || occupied[index] == 2) {
+				//table[index].id = id;
+				//table[index].sender = s;
+				//table[index].receiver = r;
+				//table[index].FromCity = fromcity;
+				//table[index].ToCity = tocity;
+				table[index] = newshipment;
+				occupied[index] = 1;
+				cout << "Shipment " << id << " inserted at index " << index << "." << endl;
+				return;
+			}
+			//else if (occupied[index] == 1 && table[index].id == id) {
+			if (occupied[index] == 1 && table[index].id == id)
+			{
+				cout << "Error: Shipment with ID " << id << " already exists." << endl;
+				return;
+			}
+			index = hashFunction(index+1);
+		} while (index != start);
+		cout << "Error: Hash Table is full. Cannot insert shipment with ID " << id << "." << endl;
 	}
-	Shipment* searchShipment(int id);
-	void removeShipment(int id);
+	Shipment* searchShipment(int id)
+	{
+		int index = hashFunction(id);
+		int start = index;
+		do {
+			if (occupied[index] == 0)
+			{
+				return nullptr;
+			}
+			if (table[index].id == id && occupied[index] == 1)
+			{
+				return &table[index];
+			 }
+			index = hashFunction(index + 1);
+		} while (index != start);
+		cout << "Not Found !!!" << endl;
+	}
+	void removeShipment(int id) {
+		int index = hashFunction(id);
+		int start = index;
+		do {
+			if (occupied[index] == 0)
+			{
+				cout << "Shipment with ID " << id << " not found for removal." << endl;
+				return;
+			}
+			if (table[index].id == id && occupied[index] == 1)
+			{
+				//table[index];
+				occupied[index] == 2;
+				cout << "Shipment with ID " << id << " removed successfully." << endl;
+				return;
+			}
+			index = hashFunction(index + 1);
+		} while (index != start);
+		cout << "Not Found !!!" << endl;
+	}
 };
 
 #endif
--- ICT PRACTICE QUESTIONS/GRAPHS.cpp
+++ ICT PRACTICE QUESTIONS/GRAPHS.cpp
@@ -1,14 +1,180 @@
-//#include <iostream>
-//#include <queue>
-//using namespace std;
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class unweightedGraphs
+{
+private:
+	int num_vertex;
+	int** adj_matrix;
+public:
+	unweightedGraphs(int vertex)
+	{
+		num_vertex = vertex;
+		adj_matrix = new int* [num_vertex];
+		for (int i = 0; i < num_vertex; i++)
+		{
+			adj_matrix[i] = new int[num_vertex];
+			for (int j = 0; j < num_vertex; j++)
+			{
+				adj_matrix[i][j] = 0;
+				//insertEdge(i, j);
+			}
+		}
+	}
+	int getNumEdges()
+	{
+		int num_edges = 0;
+		for (int i = 0; i < num_vertex; i++)
+		{
+			for (int j = 0; j < num_vertex; j++)
+			{
+				if (adj_matrix[i][j] == 1)
+				{
+					num_edges++;
+				}
+			}
+		}
+		return num_edges / 2;
+	}
+	//int getNumVertex();
+	void insertEdge(int from_v, int to_v)
+	{
+		if (from_v >= 0 && from_v <= num_vertex && to_v <= num_vertex)
+		{
+			adj_matrix[from_v][to_v] = 1;
+		}
+		else
+		{
+			cout << "invalid input";
+		}
+	}
+	void removeEdge(int from_v, int to_v)
+	{
+		if (from_v >= 0 && from_v <= num_vertex && to_v <= num_vertex)
+		{
+			adj_matrix[from_v][to_v] = 0;
+		}
+		else
+		{
+			cout << "invalid input";
+		}
+	}
+	int degree(int vertex)
+	{
+		int degree = 0;
+		for (int i = 0; i < num_vertex; i++)
+		{
+			if (adj_matrix[vertex][i] == 1)
+			{
+				degree++;
+			}
+		}
+		return degree;
+	}
+	void bfs(int s)// code for bfs
+	{
+		if (s < 0 || s > num_vertex)
+		{
+			cout << "invalid input";
+			return;
+		}
+		bool* visited = new bool[num_vertex];
+		for (int i = 0; i < num_vertex; i++)
+		{
+			visited = false;
+		}
+		queue<int>q;
+		q.push(s);
+		visited[s] = true;
+		//while (q.empty() != true)
+		while (!q.empty())
+		{
+			int vertex = q.front();
+			q.pop();
+			cout << vertex;
+			for (int i = 0; i < num_vertex; i++)
+			{
+				if (adj_matrix[vertex][i] == 1 && visited[i] == false)
+				{
+					visited[i] = false;
+					q.push(i);
+				}
+			}
+		}
+	}
+	void dfs(int u) {
+
+
+
+
+		//u->visited = true;
+		//for(each vertex of v suchthat (u,v) is E) {
+		//	if (visited = false)
+		//	{
+		//		dfs(v);
+		//	}
+		//}
+
+
+	}
+	bool shortedtPathBFS(source, destination)
+	{
+		queue <int> q;
+
+	}
+
+};
+
+// time complexity of insertEdge, removeEdge and degree is O(1)
+// time complexity of getNumEdges is O(n^2)
+// time complexity of bfs is O(n^2) for adjacency matrix representation
+// time complexity of dfs is O(n^2) for adjacency matrix representation
+// time complexity of bfs and dfs is O(n+E) for adjacency list representation
+// space complexity of adjacency matrix is O(n^2)
+// space complexity of adjacency list is O(n+E)
+// space complexity of bfs and dfs is O(n) for visited array and queue/stack
+// space complexity is worst in case of dfs and best for bfs in dense graphs that is when E is close to n^2
+// rows indicate out degree in the graph...
+// col indicate in degree in the graph...
+
+
+
+////int main()
+////{
+////	unweightedGraphs g(5);
+////	//g.graph(5);
+////	g.insertEdge(0, 1);
+////	g.insertEdge(0, 2);
+////	g.insertEdge(1, 2);
+////	g.insertEdge(2, 0);
+////	g.insertEdge(2, 3);
+////	g.insertEdge(3, 3);
+////	cout << "Number of edges: " << g.getNumEdges() << endl;
+////	cout << "Degree of vertex 2: " << g.degree(2) << endl;
+////	cout << "BFS starting from vertex 2: ";
+////	g.bfs(2);
+////	cout << endl;
+////	return 0;
+////}
 //
-//class unweightedGraphs
+//
+////DEFINE MAX VERTICES
+//class node {
+//	int vertex;
+//	node* link;
+//};
+//node* adj_list[max_Vertex]; // adjacency list representation
+//
+//
+//
+//class weightedGrahs
 //{
 //private:
 //	int num_vertex;
 //	int** adj_matrix;
 //public:
-//	unweightedGraphs(int vertex)
+//	weightedGrahs(int vertex)
 //	{
 //		num_vertex = vertex;
 //		adj_matrix = new int* [num_vertex];
@@ -35,7 +201,7 @@
 //				}
 //			}
 //		}
-//		return num_edges / 2;
+//		return num_edges;
 //	}
 //	//int getNumVertex();
 //	void insertEdge(int from_v, int to_v)
@@ -118,11 +284,6 @@
 //
 //
 //	}
-//	bool shortedtPathBFS(source, destination)
-//	{
-//		queue <int> q;
-//
-//	}
 //
 //};
 //
@@ -140,182 +301,21 @@
 //
 //
 //
-//////int main()
-//////{
-//////	unweightedGraphs g(5);
-//////	//g.graph(5);
-//////	g.insertEdge(0, 1);
-//////	g.insertEdge(0, 2);
-//////	g.insertEdge(1, 2);
-//////	g.insertEdge(2, 0);
-//////	g.insertEdge(2, 3);
-//////	g.insertEdge(3, 3);
-//////	cout << "Number of edges: " << g.getNumEdges() << endl;
-//////	cout << "Degree of vertex 2: " << g.degree(2) << endl;
-//////	cout << "BFS starting from vertex 2: ";
-//////	g.bfs(2);
-//////	cout << endl;
-//////	return 0;
-//////}
-////
-////
-//////DEFINE MAX VERTICES
-////class node {
-////	int vertex;
-////	node* link;
-////};
-////node* adj_list[max_Vertex]; // adjacency list representation
-////
-////
-////
-////class weightedGrahs
-////{
-////private:
-////	int num_vertex;
-////	int** adj_matrix;
-////public:
-////	weightedGrahs(int vertex)
-////	{
-////		num_vertex = vertex;
-////		adj_matrix = new int* [num_vertex];
-////		for (int i = 0; i < num_vertex; i++)
-////		{
-////			adj_matrix[i] = new int[num_vertex];
-////			for (int j = 0; j < num_vertex; j++)
-////			{
-////				adj_matrix[i][j] = 0;
-////				//insertEdge(i, j);
-////			}
-////		}
-////	}
-////	int getNumEdges()
-////	{
-////		int num_edges = 0;
-////		for (int i = 0; i < num_vertex; i++)
-////		{
-////			for (int j = 0; j < num_vertex; j++)
-////			{
-////				if (adj_matrix[i][j] == 1)
-////				{
-////					num_edges++;
-////				}
-////			}
-////		}
-////		return num_edges;
-////	}
-////	//int getNumVertex();
-////	void insertEdge(int from_v, int to_v)
-////	{
-////		if (from_v >= 0 && from_v <= num_vertex && to_v <= num_vertex)
-////		{
-////			adj_matrix[from_v][to_v] = 1;
-////		}
-////		else
-////		{
-////			cout << "invalid input";
-////		}
-////	}
-////	void removeEdge(int from_v, int to_v)
-////	{
-////		if (from_v >= 0 && from_v <= num_vertex && to_v <= num_vertex)
-////		{
-////			adj_matrix[from_v][to_v] = 0;
-////		}
-////		else
-////		{
-////			cout << "invalid input";
-////		}
-////	}
-////	int degree(int vertex)
-////	{
-////		int degree = 0;
-////		for (int i = 0; i < num_vertex; i++)
-////		{
-////			if (adj_matrix[vertex][i] == 1)
-////			{
-////				degree++;
-////			}
-////		}
-////		return degree;
-////	}
-////	void bfs(int s)// code for bfs
-////	{
-////		if (s < 0 || s > num_vertex)
-////		{
-////			cout << "invalid input";
-////			return;
-////		}
-////		bool* visited = new bool[num_vertex];
-////		for (int i = 0; i < num_vertex; i++)
-////		{
-////			visited = false;
-////		}
-////		queue<int>q;
-////		q.push(s);
-////		visited[s] = true;
-////		//while (q.empty() != true)
-////		while (!q.empty())
-////		{
-////			int vertex = q.front();
-////			q.pop();
-////			cout << vertex;
-////			for (int i = 0; i < num_vertex; i++)
-////			{
-////				if (adj_matrix[vertex][i] == 1 && visited[i] == false)
-////				{
-////					visited[i] = false;
-////					q.push(i);
-////				}
-////			}
-////		}
-////	}
-////	void dfs(int u) {
-////
-////
-////
-////
-////		//u->visited = true;
-////		//for(each vertex of v suchthat (u,v) is E) {
-////		//	if (visited = false)
-////		//	{
-////		//		dfs(v);
-////		//	}
-////		//}
-////
-////
-////	}
-////
-////};
-////
-////// time complexity of insertEdge, removeEdge and degree is O(1)
-////// time complexity of getNumEdges is O(n^2)
-////// time complexity of bfs is O(n^2) for adjacency matrix representation
-////// time complexity of dfs is O(n^2) for adjacency matrix representation
-////// time complexity of bfs and dfs is O(n+E) for adjacency list representation
-////// space complexity of adjacency matrix is O(n^2)
-////// space complexity of adjacency list is O(n+E)
-////// space complexity of bfs and dfs is O(n) for visited array and queue/stack
-////// space complexity is worst in case of dfs and best for bfs in dense graphs that is when E is close to n^2
-////// rows indicate out degree in the graph...
-////// col indicate in degree in the graph...
-////
-////
-////
-////int main()
-////{
-////	weightedGrahs g(5);
-////	//g.graph(5);
-////	g.insertEdge(0, 1);
-////	g.insertEdge(0, 2);
-////	g.insertEdge(1, 2);
-////	g.insertEdge(2, 0);
-////	g.insertEdge(2, 3);
-////	g.insertEdge(3, 3);
-////	cout << "Number of edges: " << g.getNumEdges() << endl;
-////	cout << "Degree of vertex 2: " << g.degree(2) << endl;
-////	cout << "BFS starting from vertex 2: ";
-////	g.bfs(2);
-////	cout << endl;
-////	return 0;
-////}
-//
+//int main()
+//{
+//	weightedGrahs g(5);
+//	//g.graph(5);
+//	g.insertEdge(0, 1);
+//	g.insertEdge(0, 2);
+//	g.insertEdge(1, 2);
+//	g.insertEdge(2, 0);
+//	g.insertEdge(2, 3);
+//	g.insertEdge(3, 3);
+//	cout << "Number of edges: " << g.getNumEdges() << endl;
+//	cout << "Degree of vertex 2: " << g.degree(2) << endl;
+//	cout << "BFS starting from vertex 2: ";
+//	g.bfs(2);
+//	cout << endl;
+//	return 0;
+//}
+
